e3bc3b146c72e743d5f024abbbb5294398209e99
amardeepk3 <amardeepk3@gmail.com>
2024-11-08 11:43:35 +0530

    env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // production: false,
   api: 'apiprod',
-  baseUrl: 'https://walrus-app-5txo6.ondigitalocean.app/api',
-  baseUrlSocket: 'https://walrus-app-5txo6.ondigitalocean.app/api'
+  baseUrl: 'https://coral-app-6ybot.ondigitalocean.app/api',
+  baseUrlSocket: 'https://coral-app-6ybot.ondigitalocean.app'
 };
